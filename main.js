36b95ce46965b31fb60e6b9e5b4e7cb68647b840
Amaransh <90545826+Amaransh@users.noreply.github.com>
2022-09-07 13:03:20 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+function preload()
+{
+
+}
+
+function setup()
+{
+    canvas = createCanvas(500,400);
+    canvas.position(70,200);
+}
+
+function draw()
+{
+
+}
+
+function take_snapshot()
+{
+    save("pick.png");
+
+}
